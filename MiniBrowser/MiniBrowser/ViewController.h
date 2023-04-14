c54f9ab0c41cabbb42da4dfb354755a475a46206
Ekko <djekko0701@gmail.com>
2023-04-14 13:40:48 +0900

    Mini Browser

--- MiniBrowser/MiniBrowser/ViewController.h
+++ MiniBrowser/MiniBrowser/ViewController.h
@@ -8,11 +8,19 @@
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate, WKNavigationDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *bookmarkSegmentedControl;
 @property (weak, nonatomic) IBOutlet WKWebView *mainWebView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
+- (IBAction)bookmarkAction:(id)sender;
+
+- (IBAction)backAction:(id)sender;
+- (IBAction)forwardAction:(id)sender;
+- (IBAction)stopAction:(id)sender;
+- (IBAction)refreshAction:(id)sender;
+
 
 @end
 
--- MiniBrowser/MiniBrowser/ViewController.m
+++ MiniBrowser/MiniBrowser/ViewController.m
@@ -12,13 +12,60 @@
 @end
 
 @implementation ViewController
-@synthesize mainWebView,urlTextField,bookmarkSegmentedControl;
+@synthesize mainWebView,urlTextField,bookmarkSegmentedControl, activityIndicatorView;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    mainWebView.navigationDelegate = self;
     NSString *urlString = @"https://www.facebook.com";
     [mainWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
     
     urlTextField.text = urlString;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSString *urlString = urlTextField.text;
+    
+    if(![urlString hasPrefix:@"http://"]) {
+        urlString = [[NSString alloc]initWithFormat:@"https://%@", urlString];
+    }
+    
+    [mainWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    [textField resignFirstResponder];
+   
+    return YES;
+}
+
+- (IBAction)refreshAction:(id)sender {
+    [mainWebView reload];
+}
+
+- (IBAction)stopAction:(id)sender {
+    [mainWebView stopLoading];
+}
+
+- (IBAction)forwardAction:(id)sender {
+    [mainWebView goForward];
+}
+
+- (IBAction)backAction:(id)sender {
+    [mainWebView goBack];
+}
+
+- (IBAction)bookmarkAction:(id)sender {
+    NSString *bookmarkURL = [bookmarkSegmentedControl titleForSegmentAtIndex: bookmarkSegmentedControl.selectedSegmentIndex];
+    NSString *urlString = [[NSString alloc]initWithFormat:@"https://www.%@.com",bookmarkURL];
+    [mainWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    
+    urlTextField.text = urlString;
+    
+}
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    [activityIndicatorView startAnimating];
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    [activityIndicatorView stopAnimating];
 }
 
 @end
